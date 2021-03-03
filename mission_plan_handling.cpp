04f4fccacb4e7a06ac446b04dcb351325e386458
DRGladwell <drg1@hw.ac.uk>
2021-03-03 21:31:26 +0000

    Initialisation of MAVSDK testing

---
+++ mission_plan_handling.cpp
@@ -0,0 +1,386 @@
+/*
+Code will clone current mission plan, upload a mission plan and segment a mission plan.
+*/
+
+// variable to hold downloaded mission plan
+
+// variable to hold current step in mission plan
+
+// variable to hold new mission plan
+
+// list of waypoint types (Not sure if requiered)
+// list_of_waypoint_types = ["normal", "clockwise", "counterclockwise", "drop_package", "glide", "land_no_power",
+//                           "automatic_take_off", "waypoint_passive_search", "waypoint_active_search"]
+
+// method to download current mission plan
+
+// method to upload new mission plan (including the starting step)
+
+// method to segment a mission plan. Return a list of waypoints with waypoint types. Unlear how to do currently
+
+// method to check current mission state. Return current waypoint type and distance to next waypoint.
+
+
+
+
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/mission/mission.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+
+#include <functional>
+#include <future>
+#include <iostream>
+
+
+#define ERROR_CONSOLE_TEXT "\033[31m" // Turn text on console red
+#define TELEMETRY_CONSOLE_TEXT "\033[34m" // Turn text on console blue
+#define NORMAL_CONSOLE_TEXT "\033[0m" // Restore normal console colour
+
+using namespace mavsdk;
+using namespace std::placeholders; // for `_1`
+using namespace std::chrono; // for seconds(), milliseconds()
+using namespace std::this_thread; // for sleep_for()
+
+// Handles Action's result
+inline void handle_action_err_exit(Action::Result result, const std::string& message);
+// Handles Mission's result
+inline void handle_mission_err_exit(Mission::Result result, const std::string& message);
+// Handles Connection result
+inline void handle_connection_err_exit(ConnectionResult result, const std::string& message);
+
+static Mission::MissionItem make_mission_item(
+    double latitude_deg,
+    double longitude_deg,
+    float relative_altitude_m,
+    float speed_m_s,
+    bool is_fly_through,
+    float gimbal_pitch_deg,
+    float gimbal_yaw_deg,
+    Mission::MissionItem::CameraAction camera_action);
+
+void usage(std::string bin_name)
+{
+    std::cout << NORMAL_CONSOLE_TEXT << "Usage : " << bin_name << " <connection_url>" << std::endl
+              << "Connection URL format should be :" << std::endl
+              << " For TCP : tcp://[server_host][:server_port]" << std::endl
+              << " For UDP : udp://[bind_host][:bind_port]" << std::endl
+              << " For Serial : serial:///path/to/serial/dev[:baudrate]" << std::endl
+              << "For example, to connect to the simulator use URL: udp://:14540" << std::endl;
+}
+
+
+int main(int argc, char** argv)
+{
+    Mavsdk mavsdk;
+
+    {
+
+        // CONNECT TO NEW UAV (SYSTEM)
+        auto prom = std::make_shared<std::promise<void>>();
+        auto future_result = prom->get_future();
+
+        std::cout << "Waiting to discover system..." << std::endl;
+
+        // print here should say ID the components that have been connected
+        mavsdk.subscribe_on_new_system([&mavsdk, prom]() {
+            const auto system = mavsdk.systems().at(0);
+
+            if (system->is_connected()) {
+                std::cout << "Discovered system" << std::endl;
+                prom->set_value();
+            } else {
+                std::cout << "System timed out" << std::endl;
+                std::cout << "Exiting." << std::endl;
+                exit(0);
+            }
+        });
+
+        std::string connection_url;
+        ConnectionResult connection_result;
+
+        // (probably connection to the url stated in the terminal command line)
+        if (argc == 2) {
+            connection_url = argv[1];
+            connection_result = mavsdk.add_any_connection(connection_url);
+        } else {
+            usage(argv[0]);
+            return 1;
+        }
+
+        // display that the connection was a failure
+        if (connection_result != ConnectionResult::Success) {
+            std::cout << ERROR_CONSOLE_TEXT << "Connection failed: " << connection_result
+                      << NORMAL_CONSOLE_TEXT << std::endl;
+            return 1;
+        }
+
+        future_result.get();
+    }
+    
+    // SYSTEM HAS BEEN CONNECTED AND WE ASSOCIATE MAVSDK STUFF TO IT
+    auto system = mavsdk.systems().at(0);
+    auto action = Action{system};
+    auto mission = Mission{system};
+    auto telemetry = Telemetry{system};
+
+    // check telemetry.health_all_okay to see if the UAV is operational
+    while (!telemetry.health_all_ok()) {
+        std::cout << "Waiting for system to be ready" << std::endl;
+        sleep_for(seconds(1));
+    }
+
+    std::cout << "System ready" << std::endl;
+    std::cout << "Creating and uploading mission" << std::endl;
+
+    // CREATING A MISSION
+
+    // data type that holds the mission items
+    std::vector<Mission::MissionItem> mission_items;
+
+    mission_items.push_back(make_mission_item(
+        47.398170327054473,
+        8.5456490218639658,
+        10.0f,
+        5.0f,
+        false,
+        20.0f,
+        60.0f,
+        Mission::MissionItem::CameraAction::None));
+
+     mission_items.push_back(make_mission_item(
+        47.398241338125118,
+        8.5455360114574432,
+        10.0f,
+        2.0f,
+        true,
+        0.0f,
+        -60.0f,
+        Mission::MissionItem::CameraAction::TakePhoto));
+
+    mission_items.push_back(make_mission_item(
+        47.398139363821485,
+        8.5453846156597137,
+        10.0f,
+        5.0f,
+        true,
+        -45.0f,
+        0.0f,
+        Mission::MissionItem::CameraAction::StartVideo));
+
+    mission_items.push_back(make_mission_item(
+        47.398058617228855,
+        8.5454618036746979,
+        10.0f,
+        2.0f,
+        false,
+        -90.0f,
+        30.0f,
+        Mission::MissionItem::CameraAction::StopVideo));
+
+    mission_items.push_back(make_mission_item(
+        47.398100366082858,
+        8.5456969141960144,
+        10.0f,
+        5.0f,
+        false,
+        -45.0f,
+        -30.0f,
+        Mission::MissionItem::CameraAction::StartPhotoInterval));
+
+    mission_items.push_back(make_mission_item(
+        47.398001890458097,
+        8.5455576181411743,
+        10.0f,
+        5.0f,
+        false,
+        0.0f,
+        0.0f,
+        Mission::MissionItem::CameraAction::StopPhotoInterval));
+
+    // UPLOAD THE MISSION
+    {
+        std::cout << "Uploading mission..." << std::endl;
+        // We only have the upload_mission function asynchronous for now, so we wrap it using
+        // std::future.
+        auto prom = std::make_shared<std::promise<Mission::Result>>();
+        auto future_result = prom->get_future();
+
+        // make a mission plan object
+        Mission::MissionPlan mission_plan{};
+        // add items to the mission plan object
+        mission_plan.mission_items = mission_items;
+        // upload mission plan items
+        mission.upload_mission_async(
+            mission_plan, [prom](Mission::Result result) { prom->set_value(result); });
+
+        // check if mission plan successfully uploaded
+        const Mission::Result result = future_result.get();
+        if (result != Mission::Result::Success) {
+            std::cout << "Mission upload failed (" << result << "), exiting." << std::endl;
+            return 1;
+        }
+        std::cout << "Mission uploaded." << std::endl;
+    }
+
+    // ARM THE AIRCRAFT TO START A NEW MISSION
+
+    std::cout << "Arming..." << std::endl;
+    const Action::Result arm_result = action.arm();
+    handle_action_err_exit(arm_result, "Arm failed: ");
+    std::cout << "Armed." << std::endl;
+
+    std::atomic<bool> want_to_pause{false};
+    // Before starting the mission, we want to be sure to subscribe to the mission progress.
+    mission.subscribe_mission_progress([&want_to_pause](Mission::MissionProgress mission_progress) {
+        std::cout << "Mission status update: " << mission_progress.current << " / "
+                  << mission_progress.total << std::endl;
+
+        if (mission_progress.current >= 2) {
+            // We can only set a flag here. If we do more request inside the callback,
+            // we risk blocking the system.
+            want_to_pause = true;
+        }
+    });
+
+
+    // START THE NEW MISSION
+    {
+        std::cout << "Starting mission." << std::endl;
+        auto prom = std::make_shared<std::promise<Mission::Result>>();
+        auto future_result = prom->get_future();
+        mission.start_mission_async([prom](Mission::Result result) {
+            prom->set_value(result);
+            std::cout << "Started mission." << std::endl;
+        });
+
+        const Mission::Result result = future_result.get();
+        handle_mission_err_exit(result, "Mission start failed: ");
+    }
+    
+    // CONTROL TO PAUSE THE MISSION
+    while (!want_to_pause) {
+        sleep_for(seconds(1));
+    }
+
+    {
+        auto prom = std::make_shared<std::promise<Mission::Result>>();
+        auto future_result = prom->get_future();
+
+        std::cout << "Pausing mission..." << std::endl;
+        mission.pause_mission_async([prom](Mission::Result result) { prom->set_value(result); });
+
+        const Mission::Result result = future_result.get();
+        if (result != Mission::Result::Success) {
+            std::cout << "Failed to pause mission (" << result << ")" << std::endl;
+        } else {
+            std::cout << "Mission paused." << std::endl;
+        }
+    }
+    // Pause for 5 seconds.
+    sleep_for(seconds(5));
+
+    // Then continue.
+    {
+        auto prom = std::make_shared<std::promise<Mission::Result>>();
+        auto future_result = prom->get_future();
+
+        std::cout << "Resuming mission..." << std::endl;
+        mission.start_mission_async([prom](Mission::Result result) { prom->set_value(result); });
+
+        const Mission::Result result = future_result.get();
+        if (result != Mission::Result::Success) {
+            std::cout << "Failed to resume mission (" << result << ")" << std::endl;
+        } else {
+            std::cout << "Resumed mission." << std::endl;
+        }
+    } 
+    while (!mission.is_mission_finished().second) {
+        sleep_for(seconds(1));
+    }
+    // RETURN TO HOME 
+    {
+        // We are done, and can do RTL to go home.
+        std::cout << "Commanding RTL..." << std::endl;
+        const Action::Result result = action.return_to_launch();
+        if (result != Action::Result::Success) {
+            std::cout << "Failed to command RTL (" << result << ")" << std::endl;
+        } else {
+            std::cout << "Commanded RTL." << std::endl;
+        }
+    }
+    
+   
+     // We need to wait a bit, otherwise the armed state might not be correct yet.
+    sleep_for(seconds(2));
+
+    while (telemetry.armed()) {
+        // Wait until we're done.
+        sleep_for(seconds(1));
+    }
+    std::cout << "Disarmed, exiting." << std::endl;
+}
+
+Mission::MissionItem make_mission_item(
+    double latitude_deg,
+    double longitude_deg,
+    float relative_altitude_m,
+    float speed_m_s,
+    bool is_fly_through,
+    float gimbal_pitch_deg,
+    float gimbal_yaw_deg,
+    Mission::MissionItem::CameraAction camera_action)
+{
+    Mission::MissionItem new_item{};
+    new_item.latitude_deg = latitude_deg;
+    new_item.longitude_deg = longitude_deg;
+    new_item.relative_altitude_m = relative_altitude_m;
+    new_item.speed_m_s = speed_m_s;
+    new_item.is_fly_through = is_fly_through;
+    new_item.gimbal_pitch_deg = gimbal_pitch_deg;
+    new_item.gimbal_yaw_deg = gimbal_yaw_deg;
+    new_item.camera_action = camera_action;
+    return new_item;
+}
+
+inline void handle_action_err_exit(Action::Result result, const std::string& message)
+{
+    if (result != Action::Result::Success) {
+        std::cerr << ERROR_CONSOLE_TEXT << message << result << NORMAL_CONSOLE_TEXT << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+inline void handle_mission_err_exit(Mission::Result result, const std::string& message)
+{
+    if (result != Mission::Result::Success) {
+        std::cerr << ERROR_CONSOLE_TEXT << message << result << NORMAL_CONSOLE_TEXT << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Handles connection result
+inline void handle_connection_err_exit(ConnectionResult result, const std::string& message)
+{
+    if (result != ConnectionResult::Success) {
+        std::cerr << ERROR_CONSOLE_TEXT << message << result << NORMAL_CONSOLE_TEXT << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+
+
+
+class mission_plan
+{   
+    public:
+
+        // code to get mission plan from ground station. This is a MAVSDK plan and an array fo strings
+        int get_mission_plan()
+        {
+            
+            return 1;
+        }
+
+};
