1114bd6dbe83e56088c82a085ae9dd1852e6629f
DRGladwell <drg1@hw.ac.uk>
2021-03-03 21:48:13 +0000

    first commit

---
+++ build/CMakeFiles/modify_plan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modify_plan.dir/modify_mission_plan.cpp.o"
+  "modify_plan"
+  "modify_plan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modify_plan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
